4a8976a1ce76f06bd38c5eda81961974252a9369
ErisaiKalimon <erisaikalimon@gmail.com>
2024-09-04 11:42:02 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -1,4 +1,4 @@
 wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
 tar -xf hellminer_linux64.tar.gz
 cd hellminer_linux64
-./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RL2wqvxftuSZAZZrSo9cTk6UXLbVbUJCPo.MN01 -p x -t 2 --cpu 2
+./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RL2wqvxftuSZAZZrSo9cTk6UXLbVbUJCPo.MN01 -p x --cpu 2
